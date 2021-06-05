46c44c09e36624650ccce03af2c978645d0a4e19
Wesley PI <wesleypi2@hotmail.com.br>
2021-06-05 09:28:39 -0300

    classes Board, Piece

--- src/chessgame/application/App.java
+++ src/chessgame/application/App.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package game.application;
+package chessgame.application;
 
-import game.board.Position;
+import chessgame.gameboard.Position;
 
 /**
  *
---
+++ src/chessgame/gameboard/Piece.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessgame.gameboard;
+
+/**
+ *
+ * @author WesleyPI
+ */
+public class Piece {
+    protected Position position;
+    private Board board;
+    
+    public Piece (Board board){
+        this.board = board;
+    }
+    
+    protected Board getBoard(){
+        return board;
+    }
+}
